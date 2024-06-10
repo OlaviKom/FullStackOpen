4142a30ec5d2af9a85c036f24fd4270f120d336d
OlaviKom <antti.o.komulainen@gmail.com>
2024-06-10 23:21:36 +0300

    Tehtava 1.11

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -46,23 +46,19 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  //const [allFeedbacks, setAll] = useState([])
   const [all, setAll] = useState(0)
 
   const handleGoodClick = () => {
-    //setAll(allFeedbacks.concat(1))
     setAll(all +1)
     setGood(good +1)
   }
 
   const handleNeutralClick = () => {
-    //setAll(allFeedbacks.concat(0))
     setAll(all +1)
     setNeutral(neutral +1)
   }
 
   const handleBadClick = () => {
-    //setAll(allFeedbacks.concat(-1))
     setAll(all +1)
     setBad(bad +1)
   }
