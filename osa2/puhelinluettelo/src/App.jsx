71001f6e90c3ba939228213d9ce9085e8efc627e
OlaviKom <antti.o.komulainen@gmail.com>
2024-06-13 23:57:20 +0300

    Tehtava 2.9

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,12 +1,34 @@
 import { useState } from 'react'
 
+const PersonsToShow = (props) => {
+  if (props.newName === ''){
+    return (
+      props.persons.map(person =>
+        <div key = {person.name}>{person.name} {person.number}</div>
+      )
+    )
+  } else {
+    const filteredPersons = props.persons.filter(person=> person.name.toLowerCase().includes(props.newFilterValue.toLowerCase()))
+    return(
+      filteredPersons.map(person =>
+        <div key = {person.name}>{person.name} {person.number}</div>
+      )
+    )
+  }
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [newName, setNewName] = useState('')
 
   const [newNumber, setNewNumber] = useState('')
+
+  const [newFilterValue, setNewFilterValue] = useState('')
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
@@ -16,6 +38,11 @@ const App = () => {
   const handleNumberChange = (event) => {
     console.log(event.target.value)
     setNewNumber(event.target.value)
+  }
+
+  const handleFilterValueChange = (event) => {
+    console.log(event.target.value)
+    setNewFilterValue(event.target.value)
   }
 
   const addPerson = (event) => {
@@ -32,11 +59,19 @@ const App = () => {
     } 
   }
 
+  
+
 
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter show with: <input
+        value = {newFilterValue}
+        onChange = {handleFilterValueChange}/>
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input 
@@ -56,9 +91,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person =>
-          <div key = {person.name}>{person.name} {person.number}</div>
-        )}
+        <PersonsToShow persons = {persons} newFilterValue = {newFilterValue}/>
       </div>
     </div>
   )
@@ -66,3 +99,9 @@ const App = () => {
 }
 
 export default App
+
+/*
+{persons.map(person =>
+          <div key = {person.name}>{person.name} {person.number}</div>
+        )}
+          */
