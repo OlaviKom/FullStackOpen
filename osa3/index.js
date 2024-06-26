fee5883c46b038e959c0c5d270c0f95eecbd42c7
OlaviKom <antti.o.komulainen@gmail.com>
2024-06-26 23:16:26 +0300

    3.6

--- osa3/index.js
+++ osa3/index.js
@@ -24,6 +24,8 @@ let persons = [
     }
   ]
 
+  app.use(express.json())
+
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
@@ -39,6 +41,57 @@ app.get('/info', (request, response) => {
     <p>Phonebook has info for ${personsLength}</p> 
     <p>${new Date}</p>`)
 }) 
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(person => person.id === id)
+
+  if(person){
+    response.json(person)
+  } else{
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+const generateId = () => {
+  const id = Math.floor(Math.random() * 1000)
+
+  return String(id)
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+
+  if(!body.name) {
+    return response.status(400).json({error: 'name missing'})
+  }
+  
+  if(!body.number) {
+    return response.status(400).json({error: 'number missing'})
+  }
+
+  if (persons.find(person => person.name === body.name)){
+    return response.status(403).json({error: 'name must be unique'})
+  }
+  
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
