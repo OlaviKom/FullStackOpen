b1c2aea91baf674c62e8e323317e40a14c40ffb9
OlaviKom <antti.o.komulainen@gmail.com>
2024-06-30 00:36:00 +0300

    korjaus

--- osa3/index.js
+++ osa3/index.js
@@ -69,7 +69,7 @@ app.get('/api/persons/:id', (request, response) => {
 
 app.delete('/api/persons/:id', (request, response) => {
   const id = request.params.id
-  const person = persons.filter(person => person.id !== id)
+  const persons = persons.filter(person => person.id !== id)
 
   response.status(204).end()
 })
