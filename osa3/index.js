7a74914a40a35dbf7014e20f6e7faba22e08a2eb
OlaviKom <antti.o.komulainen@gmail.com>
2024-06-29 23:23:25 +0300

    commit

--- osa3/index.js
+++ osa3/index.js
@@ -37,8 +37,9 @@ morgan.token('body', (req) => JSON.stringify(req.body))
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
+
 app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+  response.send('<h1>Puhelin luettelo</h1>')
 })
 
 
