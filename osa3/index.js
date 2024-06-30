2d7a0dc59cc7072a137faba38370f0679002d53c
OlaviKom <antti.o.komulainen@gmail.com>
2024-06-30 22:28:45 +0300

    delete fix

--- osa3/index.js
+++ osa3/index.js
@@ -69,7 +69,7 @@ app.get('/api/persons/:id', (request, response) => {
 
 app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  const persons = persons.filter(person => person.id !== id)
+  persons = persons.filter(person => person.id !== id)
 
   response.status(204).end()
 })
