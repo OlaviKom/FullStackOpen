21a0ccc594f9cc51b0346c41a2bfc0b581049b52
OlaviKom <antti.o.komulainen@gmail.com>
2024-07-07 22:48:21 +0300

    3.22

--- osa3/models/person.js
+++ osa3/models/person.js
@@ -23,7 +23,7 @@ const personSchema = new mongoose.Schema({
     type: String,
     validate: {
       validator: function(v) {
-        return /\d{2,3}-\d{7,8}/.test(v)
+        return /^\d{2,3}-\d{7,8}$/.test(v)
       },
       message: props => `${props.value} is not valid phone number`
     },
