c00c2345d25f4c3f9b825e0e539082e5c8324194
OlaviKom <antti.o.komulainen@gmail.com>
2024-07-20 00:31:36 +0300

    4.19

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -2,6 +2,7 @@ const blogsRouter = require('express').Router()
 const blog = require('../models/blog')
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
@@ -32,10 +33,22 @@ blogsRouter.put('/:id', async(request, response) => {
   response.json(updatedBlog)
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if(authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if(!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
 
-  const user = await User.findById(body.user)
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title,
